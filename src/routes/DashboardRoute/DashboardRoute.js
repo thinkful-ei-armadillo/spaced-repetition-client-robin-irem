e868223c73e70d6a8dc65984b1286534f5aab12f
Robin Khiv <robinkhiv@gmail.com>
2019-04-15 07:35:17 -0700

    started on dashboard

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -4,7 +4,11 @@ class DashboardRoute extends Component {
   render() {
     return (
       <section>
-        implement and style me
+        <h2>Turkish</h2>
+         <h2>Word To Practice</h2>
+         <h2>Total Score:</h2>
+         <p>list with words and how many correct and incorrect for each?</p>
+        <input type='button' name='start' value='Start Practicing' />
       </section>
     );
   }
